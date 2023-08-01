0e8bfb389758022e654830ca6a0441466b38fc2e
Francesco Cortella <t-fcortella@microsoft.com>
2023-08-01 18:03:59 +0200

    ok

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import whisper
-from pathlib import Path
 
 app = Flask(__name__)
 
